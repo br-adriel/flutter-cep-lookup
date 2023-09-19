fff6d18895070bc1aa130b083c1412296f31dc5f
Adriel <adriel.fsantos@outlook.com>
2023-09-18 21:45:31 -0300

    Insere guia de busca de CEP em tela home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cep_lookup/screens/home/tabs/pesquisar_cep.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,7 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ]),
         ),
         body: const TabBarView(children: [
-          Text("Tela 1"),
+          PesquisarCEPTab(),
           Text("Tela 2"),
         ]),
       ),
